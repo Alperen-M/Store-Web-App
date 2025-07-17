333afc74cb52b77550844bfcab574d3f08128bf7
Alperen Mengünoğul <alperenmengunogul1@gmail.com>
2025-07-17 16:00:50 +0300

    Update ProductDto.cs

--- WebApplication1/WebApplication1/Dtos/ProductDto.cs
+++ WebApplication1/WebApplication1/Dtos/ProductDto.cs
@@ -1,15 +1,22 @@
-﻿namespace WebApplication1.Models
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models
 {
     public class ProductDto
     {
-        public int Id { get; set; }
-
+        [Required(ErrorMessage = "Ürün adı zorunludur.")]
+        [StringLength(100)]
         public string Name { get; set; } = string.Empty;
 
+        [Required(ErrorMessage = "Fiyat zorunludur.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Fiyat pozitif olmalıdır.")]
         public decimal Price { get; set; }
 
+        [StringLength(255)]
         public string? Description { get; set; }
 
-        // FK yok!
+        [Required]
+        public int StoreId { get; set; }
     }
 }
