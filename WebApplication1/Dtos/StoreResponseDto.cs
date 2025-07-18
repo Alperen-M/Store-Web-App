c10e108a4f1007e801a5533952abc05c59c10b31
Alperen Mengünoğul <alperenmengunogul1@gmail.com>
2025-07-18 14:35:34 +0300

    Update StoreResponseDto.cs

--- WebApplication1/Dtos/StoreResponseDto.cs
+++ WebApplication1/Dtos/StoreResponseDto.cs
@@ -1,14 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using WebApplication1.Dtos;
 
-namespace WebApplication1.Dtos
+namespace WebApplication1.Models
 {
     public class StoreResponseDto
     {
         public int Id { get; set; }
+
         public string Name { get; set; } = string.Empty;
+
         public string Location { get; set; } = string.Empty;
+
         public string? Description { get; set; }
-        public List<ProductResponseDto> Products { get; set; } = new List<ProductResponseDto>();
+
+        public List<ProductResponseDto>? Products { get; set; }
+
     }
 }
+
