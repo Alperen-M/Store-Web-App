e04b08ad409136bd2e47bca5b5915b4967a0b286
Alperen Mengünoğul <alperenmengunogul1@gmail.com>
2025-07-20 22:56:22 +0300

    Update Program.cs

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -2,7 +2,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using WebApplication1.Data;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
