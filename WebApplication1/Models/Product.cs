211062fe9d717bf54ba811d85712ecb722a145db
Alperen Mengünoğul <alperenmengunogul1@gmail.com>
2025-07-21 15:59:50 +0300

    Update Product.cs

--- WebApplication1/Models/Product.cs
+++ WebApplication1/Models/Product.cs
@@ -4,17 +4,12 @@ namespace WebApplication1.Models
 {
     public class Product
     {
-        [Key]
         public int Id { get; set; }
-
-        public string Name { get; set; } = string.Empty;
-
+        public string Name { get; set; } = null!;
         public decimal Price { get; set; }
-
         public string? Description { get; set; }
 
-        public int StoreId { get; set; } // FK veritabanında durur, client görmez
-
-        public Stores? Store { get; set; }
+        public int StoreId { get; set; }
+        public Store Store { get; set; } = null!;
     }
 }
