a325418f026ed545b111d67650b2b613ec402e93
Alperen Mengünoğul <alperenmengunogul1@gmail.com>
2025-07-28 14:46:26 +0300

    Update StoreCreateDto.cs

--- WebApplication1/Dtos/StoreCreateDto.cs
+++ WebApplication1/Dtos/StoreCreateDto.cs
@@ -4,7 +4,10 @@ using System.Collections.Generic;
 public class StoreCreateDto
 {
     public string Name { get; set; } = string.Empty;
+    
     public string Location { get; set; } = string.Empty;
+    
     public string? Description { get; set; }
+    
     public List<ProductCreateDto> Products { get; set; } = new();
 }
