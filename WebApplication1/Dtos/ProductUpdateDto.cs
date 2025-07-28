86bb5ad6868b5dccc19dbca5fd51af4f32928a7d
Alperen Mengünoğul <alperenmengunogul1@gmail.com>
2025-07-28 14:46:18 +0300

    Update ProductUpdateDto.cs

--- WebApplication1/Dtos/ProductUpdateDto.cs
+++ WebApplication1/Dtos/ProductUpdateDto.cs
@@ -3,7 +3,9 @@
     public class ProductUpdateDto
     {
         public string Name { get; set; } = string.Empty;
+        
         public decimal Price { get; set; }
+        
         public string? Description { get; set; }
     }
 }
